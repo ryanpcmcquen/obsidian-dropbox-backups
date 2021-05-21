97da4798cbbeb1df4ee91f94c6a09fd356340040
Ryan McQuen <rpcm@linux.com>
2021-05-21 16:06:44 -0700

    It is broken.
    Signed-off-by: Ryan McQuen <rpcm@linux.com>

--- main.ts
+++ main.ts
@@ -1,112 +1,105 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { Dropbox } from "dropbox";
 
-interface MyPluginSettings {
-	mySetting: string;
+// interface MyPluginSettings {
+//  mySetting: string;
+// }
+
+// const DEFAULT_SETTINGS: MyPluginSettings = {
+//  mySetting: "default",
+// };
+
+export default class DropboxBackups extends Plugin {
+    // settings: MyPluginSettings;
+
+    async onload() {
+        console.log("loading plugin");
+
+        // await this.loadSettings();
+
+        this.addRibbonIcon("dice", "Backup to Dropbox", async () => {
+            const vaultPath = this.app.vault.getName();
+            const start = performance.now();
+            const allFiles = await Promise.all(
+                this.app.vault.getFiles().map(async (tfile) => {
+                    const fileContents = await this.app.vault.read(tfile);
+                    return {
+                        path: `${vaultPath}/${tfile.path}`,
+                        contents: fileContents,
+                    };
+                })
+            );
+            console.log(allFiles);
+            console.log(performance.now() - start);
+
+            console.log(Dropbox);
+        });
+
+        // this.addSettingTab(new SampleSettingTab(this.app, this));
+
+        // this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    }
+
+    onunload() {
+        console.log("unloading plugin");
+    }
+
+    // async loadSettings() {
+    //  this.settings = Object.assign(
+    //      {},
+    //      DEFAULT_SETTINGS,
+    //      await this.loadData()
+    //  );
+    // }
+
+    // async saveSettings() {
+    //  await this.saveData(this.settings);
+    // }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+// class SampleModal extends Modal {
+//  constructor(app: App) {
+//      super(app);
+//  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+//  onOpen() {
+//      let { contentEl } = this;
+//      contentEl.setText("Woah!");
+//  }
 
-	async onload() {
-		console.log('loading plugin');
+//  onClose() {
+//      let { contentEl } = this;
+//      contentEl.empty();
+//  }
+// }
 
-		await this.loadSettings();
+// class SampleSettingTab extends PluginSettingTab {
+//     plugin: MyPlugin;
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+//     constructor(app: App, plugin: MyPlugin) {
+//         super(app, plugin);
+//         this.plugin = plugin;
+//     }
 
-		this.addStatusBarItem().setText('Status Bar Text');
+//     display(): void {
+//         let { containerEl } = this;
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+//         containerEl.empty();
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+//         containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+//         new Setting(containerEl)
+//             .setName("Setting #1")
+//             .setDesc("It's a secret")
+//             .addText((text) =>
+//                 text
+//                     .setPlaceholder("Enter your secret")
+//                     .setValue("")
+//                     .onChange(async (value) => {
+//                         console.log("Secret: " + value);
+//                         this.plugin.settings.mySetting = value;
+//                         await this.plugin.saveSettings();
+//                     })
+//             );
+//     }
+// }
---
+++ upgrade-assets.cmd
@@ -0,0 +1,2 @@
+curl https://cdn.jsdelivr.net/npm/dropbox@9/dist/Dropbox-sdk.min.js -o assets/Dropbox-sdk.min.js
+curl https://raw.githubusercontent.com/dropbox/dropbox-sdk-js/main/examples/javascript/utils.js -o assets/Dropbox-utils.js
